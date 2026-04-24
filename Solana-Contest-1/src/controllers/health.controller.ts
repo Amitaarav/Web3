4ede0f90794d081565794d5c045a8c1781eb3602
Amit Kumar Gupta <108369448+Amitaarav@users.noreply.github.com>
2026-04-24 22:32:11 +0530

    Delete Solana-Contest-1 directory

--- Solana-Contest-1/src/controllers/health.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const getHealth = (_req: Request, res: Response) => {
-    res.status(200).json({ status: "ok" });
-};
--- Solana-Contest-1/src/routes/vesting.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as vestingController from "../controllers/vesting.controller";
-
-const router = Router({ mergeParams: true });
-
-router.get("/", vestingController.getVesting);
-
-export default router;
